d1aea0a581779fd0e664907586ec0ec97ce56ee6
VICUBogdanAndrei <120046294+VICUBogdanAndrei@users.noreply.github.com>
2024-01-19 17:54:59 +0200

    Update app.js

--- main/app.js
+++ main/app.js
@@ -1,6 +1,38 @@
 const compress = (a, b = true) => {
-	//TODO: implementați funcția
-	// TODO: implement the function
-}
+    if (typeof a !== 'string') {
+        throw new Error('InvalidType');
+    }
 
-module.exports = compress
+    if (b) {
+        // Compresie
+        let result = '';
+        let count = 1;
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] === a[i + 1]) {
+                count++;
+            } else {
+                result += a[i] + String(count);
+                count = 1;
+            }
+        }
+        return result;
+    } else {
+        // Decompresie
+        let result = '';
+        let i = 0;
+        while (i < a.length) {
+            let char = a[i];
+            i++;
+            let numStr = '';
+            while (i < a.length && !isNaN(a[i])) {
+                numStr += a[i];
+                i++;
+            }
+            let num = parseInt(numStr);
+            result += char.repeat(num);
+        }
+        return result;
+    }
+};
+
+module.exports = compress;
